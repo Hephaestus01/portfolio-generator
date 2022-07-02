4712cbb94a44f95fa84099aa178ab6519a2754d6
Matt <mwhlavaty@gmail.com>
2022-07-01 21:11:13 -0700

    set up inquirer

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
+import inquirer from 'inquirer'
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
 
-const [names, github] = profileDataArgs;
+// const pageHTML = generatePage(names, github);
 
-fs.writeFile('index.html', generatePage(names, github), err => {
-    if (err) throw err;
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw err;
 
-    console.log('Portfolio complete! Check out index.html to see the output!');
-});
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers))
